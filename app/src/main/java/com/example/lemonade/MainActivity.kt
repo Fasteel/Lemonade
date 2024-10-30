33e5c844c1955a0c126c52d90451896ee30e905d
Carl-MONNERA <cmonnera@regate.io>
2024-10-30 16:41:04 +0100

    initial app

---
+++ app/src/main/java/com/example/lemonade/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.example.lemonade
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import com.example.lemonade.ui.theme.LemonadeTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            LemonadeTheme {
+            }
+        }
+    }
+}
